8816a5a56585c2a9d77771a2da04bf46886a902d
bosgood <bosgood@gmail.com>
2014-12-27 17:26:50 -0500

    App info command added

---
+++ src/commands/info.js
@@ -0,0 +1,40 @@
+const Command = require('../command');
+
+var Info = Command.extend({
+  run: function() {
+    var self = this;
+    return self.http
+      .get('http://depot.dev/depot/apps/' + this.params.appId)
+      .type('json')
+      .promise()
+      .then(function(res) {
+        var app = res.body;
+        for (var prop in app) {
+          if (app.hasOwnProperty(prop)) {
+            self.logger.info(prop + ':', app[prop]);
+          }
+        }
+      })
+      .catch(function(err) {
+        self.logger.error('error while fetching app:');
+        if (err.res && err.res.status) {
+          self.logger.error('response code: ', err.res.status)
+        }
+        if (err.res && err.res.body) {
+          self.logger.error('response body: ', err.res.body);
+        }
+        if (err.stack) {
+          console.error(err.stack);
+        }
+      })
+    ;
+  }
+});
+
+module.exports = {
+  summary: 'Show app information.',
+  create: Info.create,
+  params: [
+    { name: 'appId', description: 'application id' }
+  ]
+};
