ab5554b9e70b6b189acd98783b7c7017bdb8d35d
bosgood <bosgood@gmail.com>
2014-12-28 21:26:27 -0500

    Implementing deploy-version command
    Version promotes an already existing version to be the latest version
    (which will be served when the app is requested).

---
+++ src/commands/deploy-version.js
@@ -0,0 +1,47 @@
+const util = require('util');
+
+const Command = require('../command');
+
+var DeployVersion = Command.extend({
+  run: function() {
+    var self = this;
+    this.http
+      .post(
+        util.format(
+          'http://depot.dev/depot/deploy/%s/%s',
+          self.argv.appId,
+          self.argv.versionId
+        )
+      )
+      .send({})
+      .type('json')
+      .promise()
+      .then(function(res) {
+        var appData = res.body;
+        if (appData && appData.contents) {
+          appData.contentsLength = appData.contents.length;
+          delete appData.contents;
+        }
+        self.log('info', 'Version successfully deployed.');
+        self.log('info', appData);
+        return appData;
+      })
+      .catch(function(err) {
+        self.log('error', 'Failed to deploy version.');
+        self.log('error', err);
+        if (err.stack) {
+          console.error(err.stack);
+        }
+      })
+    ;
+  }
+});
+
+module.exports = {
+  summary: 'Marks the given version as released.',
+  create: DeployVersion.create,
+  params: [
+    { name: 'appId', description: 'application id'},
+    { name: 'versionId', description: 'version id'}
+  ]
+};
