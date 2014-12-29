9987ee29933549ba96aa285e423578269a41eceb
bosgood <bosgood@gmail.com>
2014-12-28 21:07:47 -0500

    Using format helper to clean up logging a little

--- src/commands/list-versions.js
+++ src/commands/list-versions.js
@@ -1,28 +1,38 @@
+const util = require('util');
+
 const Command = require('../command');
 
 var ListVersions = Command.extend({
   run: function() {
     var self = this;
     return self.http
-      .get('http://depot.dev/depot/apps/' + this.argv.appId + '/versions')
+      .get(
+        util.format('http://depot.dev/depot/apps/%s/versions', this.argv.appId)
+      )
       .type('json')
       .promise()
       .then(function(res) {
         var versions = res.body;
         if (versions && versions.length) {
           versions.forEach(function(version) {
-            self.log('info', '*', version.versionId, '-', version.createdAt);
+            self.log(
+              'info',
+              util.format('* %s - %s', version.versionId, version.createdAt)
+            );
           });
         } else {
-          self.log('info',
-            'No versions of `' + self.argv.appId + '` currently available.'
+          self.log(
+            'info',
+            util.format(
+              'No versions of `%s` currently available.', self.argv.appId
+            )
           );
         }
         return versions;
       })
       .catch(function(err) {
         self.log('error',
-          'Error while fetching versions for `' + self.argv.appId + '`:'
+          util.format('Error while fetching versions for `%s`:', self.argv.appId)
         );
         self.log('error', err);
         if (err.stack) {
