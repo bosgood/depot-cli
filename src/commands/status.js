119bcba04e943bfc1dad2a2d63413fa2c11dbf54
bosgood <bosgood@gmail.com>
2014-12-28 20:40:13 -0500

    Formatting consistency

--- src/commands/status.js
+++ src/commands/status.js
@@ -8,13 +8,14 @@ var Status = Command.extend({
     var self = this;
     var listApps = ListApps.create();
     listApps.run()
-    .then(function(apps) {
-      self.log('info', 'depot server online:');
-      self.log('info', util.format('%d apps available.', apps.length));
-    })
-    .catch(function(err) {
-      self.log('error', 'Error while contacting server.');
-    });
+      .then(function(apps) {
+        self.log('info', 'depot server online:');
+        self.log('info', util.format('%d apps available.', apps.length));
+      })
+      .catch(function(err) {
+        self.log('error', 'Error while contacting server.');
+      })
+    ;
   }
 });
 
